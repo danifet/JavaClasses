c4e5ea3a88ffe875d41484cfa04a82a44aa09db2
Daniil Fetysov <fetisov.ds@gmail.com>
2021-05-27 19:57:11 -0400

    getters setters

--- src/day47_constructors/Address.java
+++ src/day47_constructors/Address.java
@@ -1,5 +1,48 @@
 package day47_constructors;
 
 public class Address {
+private String street;
+private String city;
+private String state;
+private String zipCode;
+private String country="USA";
+
+
+    public String toString() {
+        return street + ", " + city + ", " + state + ", " + zipCode+ ", " + country;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
 
 }
+
