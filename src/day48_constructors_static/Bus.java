a1a70f7a4723eba4aa8223cb36b196e64ac97b54
Daniil Fetysov <fetisov.ds@gmail.com>
2021-05-29 16:48:52 -0400

    calc

--- src/day48_constructors_static/Bus.java
+++ src/day48_constructors_static/Bus.java
@@ -1,11 +1,12 @@
 package day48_constructors_static;
 
-import com.sun.xml.internal.ws.api.pipe.Engine;
-
-import java.sql.Driver;
-
 public class Bus {
     Driver driver;
     Engine engine;
+
+    public String toString() {
+        return driver.getName() +" | "+ engine.getCylinders();
+    }
+
 }
 
---
+++ src/day48_constructors_static/static_examples/CalculatorTest.java
@@ -0,0 +1,10 @@
+package day48_constructors_static.static_examples;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator.add(3,1);
+
+        Calculator obj = new Calculator();
+        obj.multiply(4,25);
+    }
+}
