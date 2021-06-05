e55037b261a68ff4f4fe9e88cff0b0f61d5fd2d2
Daniil Fetysov <fetisov.ds@gmail.com>
2021-06-05 12:58:39 -0400

    test

---
+++ src/day50_inheritance/Person.java
@@ -0,0 +1,22 @@
+package day50_inheritance;
+
+public class Person {
+    String name;
+    int age;
+
+    public void walk(){
+
+    }
+
+    public void walk(){
+
+    }
+
+    public void walk(){
+
+    }
+
+    public void walk(){
+
+    }
+}
