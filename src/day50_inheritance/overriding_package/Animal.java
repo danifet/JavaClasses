6c2b8244a8b9d9b8dcf1575b35be02bf395cd9dd
Daniil Fetysov <fetisov.ds@gmail.com>
2021-06-05 17:05:41 -0400

    Inheritance

---
+++ src/day50_inheritance/overriding_package/Animal.java
@@ -0,0 +1,10 @@
+package day50_inheritance.overriding_package;
+
+public class Animal {
+    String name;
+    String type;
+
+    public void speak(){
+        System.out.println("Animal is speaking");
+    }
+}
